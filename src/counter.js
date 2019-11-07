1bc98915f80a116183392f55c3f5202d73d50fe2
Alexander Makarof <makarofalex@gmail.com>
2019-11-07 20:39:27 +0300

    added bindactionscreators

--- src/counter.js
+++ src/counter.js
@@ -22,8 +22,5 @@ const mapStateToProps = (state) => {
 	}
 };
 
-const mapDispatchToProps = (dispatch) => {
-	return bindActionCreators(actions, dispatch);
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter); 
+export default connect(mapStateToProps, actions)(Counter); 
